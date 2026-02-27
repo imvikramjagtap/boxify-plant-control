8e4007fecf8e315732f37ccca1c46fb158695051
imvikramjagtap <imvikramjagtap@gmail.com>
2026-02-27 12:34:29 -0800

    add anaky

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/layout/Layout";
+import { Analytics } from "@vercel/analytics/next"
 
 // Existing pages
 import Dashboard from "./pages/Dashboard";
@@ -35,6 +36,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics />
       <BrowserRouter>
         <Layout>
           <Routes>
